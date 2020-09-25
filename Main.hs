9230d88db4a768ce2fbd82ae37a98ac46e0babe1
Logan Gorence <logan@gorence.io>
2020-09-25 00:09:33 -0700

    Start handling I/O errors, clean up main loop, work with exit codes.

--- Main.hs
+++ Main.hs
@@ -1,30 +1,55 @@
 import Data.List.Split
+import Data.Typeable
+import GHC.IO.Exception
 import System.Directory
+import System.Exit
 import System.IO
+import System.IO.Error
 import System.Process
 
 main = do
+  hSetBuffering stdout NoBuffering
+  loop
+
+loop = do
   putStr "> "
   line <- getLine
 
   let splitLine = splitOn " " line
   let cleanLine = filter (\x -> x /= "") splitLine
 
-  _ <- runCmd cleanLine
+  exitCode <- runCmd cleanLine
+  if exitCode /= ExitSuccess
+    then putStrLn ("exit code " ++ (show exitCode)) else return ()
 
-  main
+  loop
 
-runCmd :: [String] -> IO ()
+ioRunCatch :: IO () -> IO ()
+ioRunCatch f = catchIOError f handler
+  where
+    handler :: IOError -> IO ()
+    handler e = do
+      case e of
+        _ | isDoesNotExistError e -> putStrLn "error: file or directory does not exist"
+        --_ | InappropriateType e -> putStrLn "error: unexpected type"
+        _ -> putStrLn ("error: unhandled io exception " ++ (show e) ++ " " ++ (show (typeOf e)))
+
+runCmd :: [String] -> IO ExitCode
 runCmd [] = do
-  return ()
+  return ExitSuccess
 runCmd ("cd" : []) = do
   path <- getHomeDirectory
-  setCurrentDirectory path
+  ioRunCatch (setCurrentDirectory path)
+  return ExitSuccess
 runCmd ("cd" : args) = do
-  setCurrentDirectory (head args)
+  ioRunCatch (setCurrentDirectory (head args))
+  return ExitSuccess
 runCmd ("about" : _) = do
   putStrLn "hsell shell"
+  return ExitSuccess
+runCmd ("exit" : _) = do
+  exitSuccess
+  return ExitSuccess
 runCmd (cmd : args) = do
   (_, _, _, ph) <- createProcess (proc cmd args)
   waitForProcess ph
-  return ()
